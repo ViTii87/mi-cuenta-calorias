8289b7d5d770bd60e9ad260e806cc029282e5476
Victor <victormel87@gmail.com>
2016-01-08 10:38:09 +0100

    Añadido comentarios

--- Alimento.java
+++ Alimento.java
@@ -15,11 +15,9 @@ public class Alimento
     private float carbohidratos;
     // Atributo que nos permite guardar la cantidad de grasas del alimiento
     private float grasas;
-    // Atributo que nos permite guardar la cantidad de calorias del alimiento (esta pendiente)
-    private float calorias;
     // Atributo qu enos guarda el componente mayoritario del alimento (esta pendiente)
     private String componenteMayoritario;
-    
+
     /**
      * Constructor que nos permite crear un alimento que tiene por parametros el nombre
      * las proteinas, los carbohidratos y grasas.
@@ -30,5 +28,57 @@ public class Alimento
         this.carbohidratos = carbohidratos;
         this.grasas = grasas;
     }
- 
+
+    /**
+     * Metodo para calcular las estadisticas de un alimento.
+     */
+    public void muestraDatos(){
+        float calorias = (proteinas * 4) + (carbohidratos * 4) + (grasas * 9);
+
+        System.out.println("Nombre:\t\t\t\t\t" + nombre);
+        System.out.println("Proteinas por cada 100 gramos: \t\t" + proteinas);
+        System.out.println("Carbohidratos por cada 100 gramos: \t" + carbohidratos);
+        System.out.println("Grasas por cada 100 gramos: \t\t" + grasas);
+        System.out.println("Calorias:\t\t\t\t" + calorias);
+        if(proteinas == carbohidratos && carbohidratos == grasas){
+            System.out.println("Componente/s mayoritario/s: \t\t Proteinas, Carbohidratos, Grasas");
+        }
+        else{
+            if(proteinas == carbohidratos){
+                 System.out.println("Componente/s mayoritario/s: \t\t Proteinas, Carbohidratos");
+            }
+            else{
+                if(proteinas == grasas){
+                    System.out.println("Componente/s mayoritario/s: \t\t Proteinas, Grasas");
+                }
+                else{
+                    if(carbohidratos == grasas){
+                        System.out.println("Componente/s mayoritario/s: \t\t Carbohidratos, Grasas");
+                    }
+                    else{
+                        if(proteinas > carbohidratos){
+                            System.out.println("Componente/s mayoritario/s: \t\t Proteinas");
+                        }
+                        else{
+                            if(proteinas > grasas){
+                                System.out.println("Componente/s mayoritario/s: \t\t Proteinas");
+                            }
+                            else{
+                                if(proteinas < carbohidratos){
+                                    System.out.println("Componente/s mayoritario/s: \t\t Carbohidratos");
+                                }
+                                else{
+                                    if(carbohidratos < grasas){
+                                        System.out.println("Componente/s mayoritario/s: \t\t Grasas");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+    }
 }
